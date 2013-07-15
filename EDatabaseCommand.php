a6e96578631adb4826d95d981d68cbb007189e7c
Sarunas <screpito@gmail.com>
2013-07-15 10:07:12 +0100

    Change the new line

--- EDatabaseCommand.php
+++ EDatabaseCommand.php
@@ -264,8 +264,7 @@ EOS;
       }
 
       foreach ($table->foreignKeys as $name => $foreignKey) {
-        $code .= $this->indent(3) . "\$this->addForeignKey(
-         'fk_{$this->firstLetters($table->name)}_{$this->firstLetters($foreignKey[0])}_{$name}', '{$table->name}', '{$name}', '{$foreignKey[0]}', '{$foreignKey[1]}', 'CASCADE', 'CASCADE'); // FIX RELATIONS \n";
+        $code .= $this->indent(3) . "\$this->addForeignKey('fk_{$this->firstLetters($table->name)}_{$this->firstLetters($foreignKey[0])}_{$name}', '{$table->name}', '{$name}', '{$foreignKey[0]}', '{$foreignKey[1]}', 'CASCADE', 'CASCADE'); // FIX RELATIONS \n";
       }
 
       if(!$this->ignoreSQLiteChecks){
